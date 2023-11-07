76f941ecc432d8e5b7c632a882e472bcfd5b52ac
majipa <97274262+majipa007@users.noreply.github.com>
2023-11-08 00:19:04 +0530

    creating classes for main.py
    The class still contains alot of errros to be fixed:
    raceback (most recent call last):
      File "/home/sukuna/PycharmProjects/AdancedCV/venv/main.py", line 45, in <module>
        main()
      File "/home/sukuna/PycharmProjects/AdancedCV/venv/main.py", line 41, in main
        start.angle()
      File "/home/sukuna/PycharmProjects/AdancedCV/venv/main.py", line 33, in angle
        detector.findAngle(self.img, 12, 14, 16, True)
    NameError: name 'detector' is not defined

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
-import poseEstimation as pe
-import time
 import cv2 as cv
+import mediapipe as mp
+import time
+import poseEstimationModule as pm
 
+class runner:
+    def __init__(self):
+        self.cTime = 0
+        self.pTime = 0
+        self.img = None
+        self.lmlist = None
+    def angle(self):
+        if len(self.lmlist) != 0:
+            detector.findAngle(self.img, 12, 14, 16, True)
+            detector.findAngle(self.img, 11, 13, 15, True)
+    def run(self):
+        detector = pm.poseDetector()
+        cap = cv.VideoCapture(0)
+        while True:
+            success, self.img = cap.read()
+            self.img = cv.flip(self.img, 1)
+            self.img = cv.resize(self.img, (800, 600))
+            self.img = detector.findPose(self.img, False)
+            self.cTime = time.time()
+            fps = 1 / (self.cTime - self.pTime)
+            self.pTime = self.cTime
+            cv.putText(self.img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3,
+                       (255, 0, 255), 3)
+            self.img = detector.findPose(self.img, False)
+            self.lmlist = detector.findPosition(self.img, False)
+
+            cv.imshow('img', self.img)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
 
 
 
 def main():
-    pTime = 0
-    cTime = 0
-    print("hello1")
-    cap = cv.VideoCapture(0)
-    detector = pe.poseDetector()
-    print("hello2")
-    while True:
-        success, img = cap.read()
-        img = detector.findPose(img)
-        lmList = detector.findPosition(img, draw=True)
-        if len(lmList) != 0:
-            print(lmList[4])
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3,
-                   (255, 0, 255), 3)
-        cv.imshow("img", img)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+    print('hello')
+    start = runner()
+    start.run()
+    start.angle()
 
 
 if __name__ == '__main__':
