20052665a3e81273cc5f5fd45b24a6018d45a009
majipa <97274262+majipa007@users.noreply.github.com>
2023-11-07 18:00:23 +0530

    Update poseEstimationModule.py
    changes made

--- poseEstimationModule.py
+++ poseEstimationModule.py
@@ -6,18 +6,22 @@ import math
 
 class poseDetector:
     def __init__(self):
+        self.results = None
         self.mpPose = mp.solutions.pose
         self.pose = self.mpPose.Pose()
         self.mpDraw = mp.solutions.drawing_utils
+
     def findPose(self, img, draw=True):
-        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        self.results = self.pose.process(imgRGB)
+        # imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        img = cv.resize(img, (1000, 500))
+        self.results = self.pose.process(img)
         # print(results.multi_hand_landmarks)
         if self.results.pose_landmarks:
             if draw:
-                self.mpDraw.draw_landmarks(img,self.results.pose_landmarks,
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
                                            self.mpPose.POSE_CONNECTIONS)
         return img
+
     def findPosition(self, img, draw=True):
         self.lmList = []
         if self.results.pose_landmarks:
@@ -32,24 +36,24 @@ class poseDetector:
                     cv.circle(img, (cx, cy), 4, (0, 225, 0), cv.FILLED)
         return self.lmList
 
-    def findAngle(self, img, p1, p2, p3, draw = True):
-        #getting the landmarks
+    def findAngle(self, img, p1, p2, p3, draw=True):
+        # getting the landmarks
         x1, y1 = self.lmList[p1][1:]
         x2, y2 = self.lmList[p2][1:]
         x3, y3 = self.lmList[p3][1:]
-        angle = math.degrees(math.atan2(y3-y2, x3-x2)-
-                           math.atan2(y1-y2, x1-x2))
-        if(angle >180):
-            angle = 360-angle
-        if(angle<0):
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+                             math.atan2(y1 - y2, x1 - x2))
+        if (angle > 180):
+            angle = 360 - angle
+        if (angle < 0):
             angle = -angle
-        #calculating the angle
+        # calculating the angle
         print(angle)
 
-        #drawing in the image
+        # drawing in the image
         if draw:
-            cv.line(img, (x1, y1), (x2, y2),(0, 225, 0), 2)
-            cv.line(img, (x3, y3), (x2, y2),(0, 225, 0), 2)
+            cv.line(img, (x1, y1), (x2, y2), (0, 225, 0), 2)
+            cv.line(img, (x3, y3), (x2, y2), (0, 225, 0), 2)
             cv.circle(img, (x1, y1), 4, (0, 225, 0), cv.FILLED)
             cv.circle(img, (x1, y1), 10, (0, 225, 0), 2)
             cv.circle(img, (x2, y2), 4, (0, 225, 0), cv.FILLED)
@@ -57,33 +61,8 @@ class poseDetector:
             cv.circle(img, (x3, y3), 4, (0, 225, 0), cv.FILLED)
             cv.circle(img, (x3, y3), 10, (0, 225, 0), 2)
 
-        #displaying the angle value
-        cv.putText(img, str(int(angle)), (x2-70, y2-20), cv.FONT_HERSHEY_PLAIN, 2,
-                    (255, 0, 255), 3 )
-
-def main():
-    pTime = 0
-    cTime = 0
-    cap = cv.VideoCapture('/home/sukuna/Desktop/hello.mp4')
-    print("hello1")
-    detector = poseDetector()
-    print("hello2")
-    while True:
-        success, img = cap.read()
-        img = detector.findPose(img)
-        lmList = detector.findPosition(img, draw = True)
-        if len(lmList) != 0:
-            print(lmList[4])
-        cTime = time.time()
-        fps = 1/(cTime-pTime)
-        pTime = cTime
-        cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 255), 3)
-        cv.imshow("img", img)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+        # displaying the angle value
+        cv.putText(img, str(int(angle)), (x2 - 70, y2 - 20), cv.FONT_HERSHEY_PLAIN, 2,
+                   (255, 0, 255), 3)
 
 
-
-if __name__=="__main__":
-    main()
