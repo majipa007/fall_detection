13c0d915d9bbb41f4f9dc995538fc1170974936a
majipa <97274262+majipa007@users.noreply.github.com>
2023-11-12 15:27:10 +0530

    Solving the no angle calculation problem.

--- main.py
+++ main.py
@@ -11,24 +11,26 @@ class runner:
         self.lmlist = None
     def angle(self):
         if len(self.lmlist) != 0:
-            detector.findAngle(self.img, 12, 14, 16, True)
-            detector.findAngle(self.img, 11, 13, 15, True)
-    def run(self):
-        detector = pm.poseDetector()
+            self.detector.findAngle(self.img, 12, 14, 16, True)
+            self.detector.findAngle(self.img, 11, 13, 15, True)
+    def run(self, ang = True):
+        self.detector = pm.poseDetector()
         cap = cv.VideoCapture(0)
         while True:
             success, self.img = cap.read()
             self.img = cv.flip(self.img, 1)
             self.img = cv.resize(self.img, (800, 600))
-            self.img = detector.findPose(self.img, False)
+            self.img = self.detector.findPose(self.img, False)
             self.cTime = time.time()
             fps = 1 / (self.cTime - self.pTime)
             self.pTime = self.cTime
+
             cv.putText(self.img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3,
                        (255, 0, 255), 3)
-            self.img = detector.findPose(self.img, False)
-            self.lmlist = detector.findPosition(self.img, False)
-
+            self.img = self.detector.findPose(self.img, False)
+            self.lmlist = self.detector.findPosition(self.img, False)
+            if ang:
+                self.angle()
             cv.imshow('img', self.img)
             if cv.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -38,8 +40,7 @@ class runner:
 def main():
     print('hello')
     start = runner()
-    start.run()
-    start.angle()
+    start.run(True)
 
 
 if __name__ == '__main__':
